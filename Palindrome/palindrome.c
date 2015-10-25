a2e43c7f2c3e90d20cdd65757444f20cc64a2996
vtykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-10-25 17:59:42 -0400

    A function to check whether a linked list is a palindrome has been added

--- Palindrome/palindrome.c
+++ Palindrome/palindrome.c
@@ -1,18 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <unistd.h>
 
 #include "palindrome.h"
 
+void usage();
+
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        fprintf(stderr, "Invalid argument\n");
-        fprintf(stderr, "Usage: %s <word>\n", argv[0]);
-        exit(EXIT_FAILURE);
+    char c;
+    bool use_linked_list = false;
+
+    opterr = 0;
+    while ((c = getopt(argc, argv, "l")) != EOF) {
+        switch (c) {
+            case 'l': use_linked_list = true; break;
+            default: usage();
+        }
     }
 
-    printf("[%s] is %s\n", argv[1], is_palindrome(argv[1]) ? "a palindrome" : "not a palindrome");
+    argc -= optind;
+    argv += optind;
+
+    if (argc != 1) {
+        fprintf(stderr, "Invalid argument\n");
+        usage();
+    }
+
+    const char *str = argv[0];
+    bool is_pal = false;
+
+    if (use_linked_list) {
+        ADS_LinkedList list = ADS_LinkedList_allocate();
+        str2ll(str, list);
+        is_pal = is_ll_palindrome(list);
+        ADS_LinkedList_destroy(list);
+    } else {
+        is_pal = is_palindrome(str);
+    }
+
+    printf("[%s] is %s\n", str, is_pal ? "a palindrome" : "not a palindrome");
 
     exit(EXIT_SUCCESS);
+}
+
+void usage()
+{
+    fprintf(stderr, "Usage: palindrome [-l] <word>\n");
+    fprintf(stderr, "  -l  convert string to linked list\n");
+    exit(EXIT_FAILURE);
 }
--- Palindrome/palindrome.h
+++ Palindrome/palindrome.h
@@ -2,6 +2,8 @@
 #define PALINDROME_H
 
 #include <assert.h>
+#include "linkedlist.h"
+#include "linkedstack.h"
 
 bool is_palindrome(const char *str)
 {
@@ -23,6 +25,61 @@ bool is_palindrome(const char *str)
     // If we exit 'while' above having 'i'
     // passed 'j' - the word is palindrome
     return j < i;
+}
+
+// Converts string to linke list of chars
+void str2ll(const char *str, ADS_LinkedList list)
+{
+    char *s = (char *) str;
+    while (*s != '\0')
+        ADS_LinkedList_append_node(list, s++);
+}
+
+// Checks whether a linked list of chars is a palindrome
+bool is_ll_palindrome(ADS_LinkedList list)
+{
+    // The list should not be empty
+    assert(list->size > 0);
+
+    // If list contains one node only - it's a polindrome
+    if (list->size == 1)
+        return true;
+
+    // Slow/fast runner technique will be used to determine
+    // the middle of list
+    ADS_LinkedListNode slow = list->head;
+    ADS_LinkedListNode fast = slow->next;
+
+    // A stack will be used to hold valus from the first
+    // half of linked list
+    ADS_LinkedStack stack = ADS_LinkedStack_allocate();
+
+    // Iterate through the list and push values from its
+    // first half to stack
+    do {
+        ADS_LinkedStack_push(stack, slow->value);
+
+        slow = slow->next;
+        fast = fast->next;
+        if (fast)
+            fast = fast->next;
+    } while (fast);
+
+    // Now slow runner points to the first element we need
+    // to check
+    do {
+        char *c = ADS_LinkedStack_pop(stack);
+        if (*c != *((char *) slow->value))
+            break;
+
+        slow = slow->next;
+    } while (slow);
+
+    ADS_LinkedStack_destroy(stack);
+
+    // If we left the do-while block above with slow runner
+    // set to NULL - it's a palindrome
+    return slow == NULL;
 }
 
 #endif // PALINDROME_H
