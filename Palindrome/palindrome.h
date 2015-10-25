8e86f314f7d183f5ed5d372c44fcdf9072f1806a
vtykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-10-24 22:09:26 -0400

    Comments have been added

--- Palindrome/palindrome.h
+++ Palindrome/palindrome.h
@@ -8,12 +8,20 @@ bool is_palindrome(const char *str)
     const char *i, *j;
     i = j = str;
 
+    // Moving j to point to last symbol of str
     while (*j != '\0') ++j; --j;
 
+    // Empty string assertion
     assert(j >= i);
 
+    // Move i from left to right and j from
+    // right to left till either pointers
+    // contain different characters or they
+    // cross
     while (*i == *j && j-- > i++);
 
+    // If we exit 'while' above having 'i'
+    // passed 'j' - the word is palindrome
     return j < i;
 }
 
